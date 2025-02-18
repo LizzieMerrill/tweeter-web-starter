250ec0c412f5f632f271dfc781657e847d9233ab
Elizabeth Merrill <eamerri2@asu.edu>
2025-02-18 11:47:51 -0700

    comment removals

--- tweeter-web/src/components/userItem/UserNavigationHook.ts
+++ tweeter-web/src/components/userItem/UserNavigationHook.ts
@@ -11,30 +11,6 @@ const useUserNavigationHook = (props: Props) =>{
     const { displayErrorMessage } = useToastListener();
       const { setDisplayedUser, currentUser, authToken } =
         useUserInfoHook();
-      //   const navigateToUser = async (event: React.MouseEvent): Promise<void> => {
-      //       event.preventDefault();
-        
-      //       try {
-      //         const alias = extractAlias(event.target.toString());
-        
-      //         const user = await presenter.getUser(authToken!, alias);
-        
-      //         if (!!user) {
-      //           if (currentUser!.equals(user)) {
-      //             setDisplayedUser(currentUser!);
-      //           } else {
-      //             setDisplayedUser(user);
-      //           }
-      //         }
-      //       } catch (error) {
-      //         displayErrorMessage(`Failed to get user because of exception: ${error}`);
-      //       }
-      //     };
-    
-      // const extractAlias = (value: string): string => {
-      //   const index = value.indexOf("@");
-      //   return value.substring(index);
-      // };
 
       const navigateToUser = async (event: React.MouseEvent): Promise<void> => {
         await presenter.navigateToUser(event);
