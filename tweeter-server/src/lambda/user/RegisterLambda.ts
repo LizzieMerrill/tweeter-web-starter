c4d1e5a4e54b05c3f03f988632a454bffa3d0cf8
Elizabeth Merrill <eamerri2@asu.edu>
2025-04-09 17:48:21 -0600

    finished debugging register and login lambdas

--- tweeter-server/src/lambda/user/RegisterLambda.ts
+++ tweeter-server/src/lambda/user/RegisterLambda.ts
@@ -1,11 +1,13 @@
-import { RegisterRequest, TweeterResponse } from "tweeter-shared";
+import { RegisterRequest, AuthResponse } from "tweeter-shared";
 import { UserService } from "../../model/service/UserService";
 
-export const handler = async (request: RegisterRequest): Promise<TweeterResponse> => {
+export const handler = async (request: RegisterRequest): Promise<AuthResponse> => {
     const userService = new UserService();
-    await userService.register(request.firstName, request.lastName, request.alias, request.password, request.userImageBytes, request.imageFileExtension);
+    const [userDto, authToken] = await userService.register(request.firstName, request.lastName, request.alias, request.password, request.userImageBytes, request.imageFileExtension);
     return {
         success: true,
-        message: null
+        message: null,
+        userDto: userDto,
+        authToken: authToken
     }
 }
